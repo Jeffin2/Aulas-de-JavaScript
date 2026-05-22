4ae8692f45dfa3417dc6822b45fca6cd82759329
Jeffin2 <jeffinpvp.games@gmail.com>
2026-05-22 19:51:53 -0300

    Organizando em pastas

--- aula 04/script.js
+++ aula 04/script.js
@@ -1,18 +1,25 @@
 //muda a cor do título para azul
 document.querySelector('h1.titulo').style.color = 'blue';
+console.log("Título alterado para azul");
 
 //altera o texto do título
 document.querySelector('h1.titulo').innerText = 'Curso de JavaScript - Editado pelo JS';
+console.log("Texto do título alterado");
 
 //altera a cor do subtítulo para verde
 document.querySelector('h2.subtitulo').style.color = 'green';
+console.log("Subtítulo alterado para verde");
 
 //altera o texto do subtítulo
 document.querySelector('h2.subtitulo').innerText = 'Bem-vindo ao curso de JavaScript - Editado pelo JS';
+console.log("Texto do subtítulo alterado");
+
 
 //altera o texto do parágrafo
 document.querySelector('p').innerText = 'Texto atualizado pelo JavaScript!';
+console.log("Texto do parágrafo alterado");
+
 
 //altera a cor do parágrafo para vermelho
 document.querySelector('p').style.color = 'red';
-
+console.log("Cor do parágrafo alterada para vermelho");
