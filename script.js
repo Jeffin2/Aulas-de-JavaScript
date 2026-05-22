e34fc8ecfcf14fd500a8eb363768b543a04a635c
Jeffin2 <jeffinpvp.games@gmail.com>
2026-05-22 18:25:54 -0300

    primeiras alterções feitas pelo JS

---
+++ script.js
@@ -0,0 +1,17 @@
+//muda a cor do título para azul
+document.querySelector('h1.titulo').style.color = 'blue';
+
+//altera o texto do título
+document.querySelector('h1.titulo').innerText = 'Curso de JavaScript - Editado pelo JS';
+
+//altera a cor do subtítulo para verde
+document.querySelector('h2.subtitulo').style.color = 'green';
+
+//altera o texto do subtítulo
+document.querySelector('h2.subtitulo').innerText = 'Bem-vindo ao curso de JavaScript - Editado pelo JS';
+
+//altera o texto do parágrafo
+document.querySelector('p').innerText = 'Texto atualizado pelo JavaScript!';
+
+//altera a cor do parágrafo para vermelho
+document.querySelector('p').style.color = 'red';
