b6ba9d1076167d0302c08fcf2827b2d17fa7ffe9
Jeffin2 <jeffinpvp.games@gmail.com>
2026-05-22 21:46:45 -0300

    mais código

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//variáveis
+nome = window.prompt("Qual é o seu nome?");
+
+
+
+
 //muda a cor do título para azul
 document.querySelector('h1.titulo').style.color = 'blue';
 console.log("Título alterado para azul");
@@ -33,7 +39,9 @@ document.querySelector('h3.sai-maldicao').style.color = 'gold';
 console.log("Cor do parágrafo alterada para ouro");
 
 
-//meu primeiro comando em JS
-window.alert("Olá, seja bem-vindo ao curso de JavaScript!");
-window.confirm("Deseja continuar?");
-window.prompt("Qual é o seu nome?");
+//meus primeiros comandos em JS
+window.alert("Olá, seja bem-vindo ao curso de JavaScript!"); //exibe um alerta para o usuário
+window.confirm("Deseja continuar?"); //exibe uma caixa de confirmação
+window.prompt("Qual é o seu nome?"); //exibe uma caixa de entrada para o usuário
+
+document.getElementsByClassName('nome')[0].innerText = "Olá, " + nome + "!"; //altera o texto do elemento com a classe "nome" para o valor da variável nome
